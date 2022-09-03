6be8e498c611dd5672cf9e5c026105701d3741b7
laudryfadian <skakmate87@gmail.com>
2022-09-03 08:42:47 +0700

    hide debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Welcome to Flutter',
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Welcome to Flutter'),
